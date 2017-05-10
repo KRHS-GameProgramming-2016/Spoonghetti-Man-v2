a8ad1b2878c659111218cb56936e95c1ec36a2f7
Ethan <EthanJ.Thompson@kearsarge.org>
2017-05-10 11:16:23 -0400

    vghn

--- Meatball.py
+++ Meatball.py
@@ -1,7 +1,8 @@
 import pygame, sys, math
 
-class Meatball(1):
+class Meatball(pygame.sprite.Sprite):
     def __init__(self, pos=[0,0], size=None):
+        pygame.sprite.Sprite.__init__(self, self.containers)
         self.image = pygame.image.load("rsc/ball/regular.png")
         if size:
             self.image = pygame.transform.scale(self.image, [size,size])
