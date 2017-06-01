be602d89b769468e03d81a9a51c2c1986b17c9f6
username112 <nathan.henderson@kearsarge.org>
2017-06-01 10:46:04 -0400

    YEp

--- Chest.py
+++ Chest.py
@@ -4,7 +4,7 @@ class Chest(pygame.sprite.Sprite):
     def __init__(self, pos, size, kind):
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.size = size
-        self.image = pygame.image.load("rsc/Chest.png")
+        self.image = pygame.image.load("rsc/Chest2.png")
         
         if size:
             self.image = pygame.transform.scale(self.image, [size, size])
