351fbeabfc77d689c85513424ad16b6091c7ef9a
Ethan <EthanJ.Thompson@kearsarge.org>
2017-06-06 11:24:47 -0400

    Diff

--- differentai.py
+++ differentai.py
@@ -30,10 +30,6 @@ class Differentai(Player):
         self.image = self.images[self.frame]
         self.rect = self.image.get_rect(center = self.rect.center)
         self.maxFrame = len(self.images) - 1
-        self.goRandomDirection()
-        self.speedx = 0
-        self.speedy = 0
-        self.speed = [self.speedx, self.speedy]
         self.radius = self.rect.width/2 -1
         self.maxFrame = len(self.images) - 1
         self.animationTimer = 0
